63849fceaad79a04c11a55e6bd19fe861b215186
Alex Korolyov <alex.korolev18@gmail.com>
2021-02-03 10:28:35 +0300

    add styled components

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ActorCard from './ActorCard'
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import { FlexGrid } from '../styled'
 
 const ActorGrid = ({ data }) => {
-  return <div>
+  return <FlexGrid>
     {
       data.map(({person}) => (
         <ActorCard 
@@ -19,7 +20,7 @@ const ActorGrid = ({ data }) => {
         />
       ))
     }
-  </div>
+  </FlexGrid>
 }
 
 export default ActorGrid
--- src/components/show/ShowGrid.js
+++ src/components/show/ShowGrid.js
@@ -2,9 +2,10 @@ import React from 'react'
 import ShowCard from './ShowCard'
 
 import IMAGE_NOT_FOUND from '../../images/not-found.png'
+import { FlexGrid } from '../styled'
 
 const ShowGrid = ({ data }) => {
-  return <div>
+  return <FlexGrid>
     {
       data.map(({show}) => (
         <ShowCard 
@@ -16,7 +17,7 @@ const ShowGrid = ({ data }) => {
         />
       ))
     }
-  </div>
+  </FlexGrid>
 }
 
 export default ShowGrid
